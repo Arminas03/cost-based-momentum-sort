103c69eda1cce965ae5a875197f37a544d4e85d7
Arminas03 <77631691+Arminas03@users.noreply.github.com>
2025-03-30 20:07:59 +0200

    Two stage momentum sort (#1)
    * formation period two-stage momentum sort
    
    * reorganizing files
    
    * rolling two-stage sorting implementation
    
    * precision fixes
    
    * shrcd fix
    
    * added Marawan's trading costs + adjusted to work with two_stage_momentum
    
    * code efficiency optimisation, randomness fix
    
    * bug quick fix
    
    * data file name change
    
    * add daily returns for each permno for each date
    
    * clean code and organisation improvements

---
+++ prep_data.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+def clean_data(data: pd.DataFrame):
+    """
+    Removes irrelevant or non-informative observations
+    """
+    data = data.dropna()
+    data = data[data["PrimaryExch"] == "N"]
+    data = data[~data["DlyRet"].apply(lambda x: isinstance(x, str))]
+
+    return data
+
+
+def adjust_data_cols(data: pd.DataFrame):
+    """
+    Adjusts data columns for easier use
+    """
+    data["DlyCalDt"] = pd.to_datetime(data["DlyCalDt"])
+    data["year"] = data["DlyCalDt"].dt.year
+    data["month"] = data["DlyCalDt"].dt.month
+    data["quoted_spread"] = (
+        2 * (data["DlyAsk"] - data["DlyBid"]) / (data["DlyAsk"] + data["DlyBid"])
+    )
+    data["DlyRet"] = data["DlyRet"].astype("float")
+
+
+def extract_data(path):
+    """
+    Reads and prepares data
+    """
+    data_cleaned = clean_data(pd.read_csv(path))
+    adjust_data_cols(data_cleaned)
+
+    return data_cleaned
+
+
+def main():
+    df = extract_data("2019-2024 v2.csv")
+    df = df.dropna()
+
+
+if __name__ == "__main__":
+    main()
---
+++ two_stage_momentum.py
@@ -0,0 +1,155 @@
+from datetime import datetime
+from prep_data import extract_data
+import pandas as pd
+import json
+import numpy as np
+import itertools
+
+rng = np.random.default_rng(1)
+
+
+def compute_return(returns):
+    """
+    Computes compound return given list of simple returns
+    """
+    final_return = 1
+    for r in returns:
+        final_return *= 1 + r
+
+    return final_return - 1
+
+
+def pick_random_day(group: pd.DataFrame, rng):
+    """
+    Picks random day from 15th until the end of the month
+    """
+    return group[-15:].sample(n=1, random_state=rng)
+
+
+def get_half_year_daily_returns(returns):
+    """
+    Returns a list of daily returns for the last six months
+    """
+    return list(itertools.chain(*returns[-6:]))
+
+
+def get_stock_returns(stock_data: pd.DataFrame):
+    """
+    Gets cumulative stock returns for each permno together with a list
+    of daily returns
+    """
+    return (
+        stock_data.groupby(["PERMNO", "month"])[["DlyRet", "quoted_spread"]]
+        .agg(
+            cumulative_return=("DlyRet", compute_return),
+            day_quoted_spread=(
+                "quoted_spread",
+                lambda group: pick_random_day(group, rng),
+            ),
+            daily_returns=("DlyRet", lambda ret: ret.tolist()),
+        )
+        .groupby(["PERMNO"])
+        .agg(
+            cumulative_return=("cumulative_return", compute_return),
+            avg_quoted_spread=("day_quoted_spread", "mean"),
+            daily_returns=("daily_returns", get_half_year_daily_returns),
+        )
+    )
+
+
+def find_momentum_split(
+    stock_data: pd.DataFrame, long_split_proportion=0.2, short_split_proportion=0.2
+):
+    """
+    Finds standard momentum strategy long and short legs
+    """
+    stock_returns = get_stock_returns(stock_data)
+
+    return (
+        stock_returns.nlargest(
+            int(len(stock_returns) * long_split_proportion), "cumulative_return"
+        ),
+        stock_returns.nsmallest(
+            int(len(stock_returns) * short_split_proportion), "cumulative_return"
+        ),
+    )
+
+
+def adjust_momentum_with_costs(long_split, short_split, cost_sensitivity):
+    """
+    Calculates cost-adjusted returns for each split
+    """
+    new_long_split = pd.DataFrame(
+        {
+            "cost_adjusted_return": long_split["cumulative_return"]
+            - cost_sensitivity * long_split["avg_quoted_spread"]
+        }
+    )
+    new_long_split["daily_returns"] = long_split["daily_returns"]
+
+    new_short_split = pd.DataFrame(
+        {
+            "cost_adjusted_return": short_split["cumulative_return"]
+            + cost_sensitivity * short_split["avg_quoted_spread"]
+        }
+    )
+    new_short_split["daily_returns"] = short_split["daily_returns"]
+
+    return new_long_split, new_short_split
+
+
+def get_final_splits(data, cost_sensitivity=1, keep_long=0.5, keep_short=0.5):
+    """
+    Gets final long and short legs based on trading costs
+    and input parameters
+    """
+    new_long_split, new_short_split = adjust_momentum_with_costs(
+        *find_momentum_split(data), cost_sensitivity
+    )
+
+    return (
+        dict(
+            new_long_split.nlargest(
+                int(len(new_long_split) * keep_long), "cost_adjusted_return"
+            )[["cost_adjusted_return", "daily_returns"]].to_dict(orient="index")
+        ),
+        dict(
+            new_short_split.nsmallest(
+                int(len(new_short_split) * keep_short), "cost_adjusted_return"
+            )[["cost_adjusted_return", "daily_returns"]].to_dict(orient="index")
+        ),
+    )
+
+
+def find_splits_per_date(data):
+    """
+    Finds the two-stage sorting long and short legs
+    """
+    splits = dict()
+
+    for date in pd.date_range(
+        start=datetime(2019, 12, 31), end=datetime(2024, 12, 31), freq="ME"
+    ):
+        print(date)
+        long_split, short_split = get_final_splits(
+            data[
+                (data["DlyCalDt"] <= date)
+                & (data["DlyCalDt"] > date - pd.DateOffset(years=1))
+            ]
+        )
+
+        splits[str(date.to_pydatetime().date())] = {
+            "long_split": long_split,
+            "short_split": short_split,
+        }
+
+    return splits
+
+
+def main():
+    with open("final_split.json", "w") as file:
+        json.dump(find_splits_per_date(extract_data("2019-2024 v2.csv")), file)
+
+
+if __name__ == "__main__":
+    main()
